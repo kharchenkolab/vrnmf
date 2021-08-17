a471357036b8326137724b3fdad27458e49758d8
Evan Biederstedt <evan.biederstedt@gmail.com>
2021-08-17 12:10:35 -0400

    update roxygen2 docs, remove vrnmf.Rcheck

--- vrnmf.Rcheck/00_pkg_src/vrnmf/R/simnmf.R
+++
@@ -1,5 +0,0 @@
-#' Simulated example of noiseless non-negative matrix factorization.
-#'
-#' A dataset containing original, noisy and decomposition matrices. Created with the internal function 'sim_factors()'
-#'
-"simnmf"
